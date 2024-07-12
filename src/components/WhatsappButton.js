92fcfb3a8bb39633c1cc9e8abf788217cdec3d95
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-07-12 22:24:50 +0530

    fourth

--- src/components/WhatsappButton.js
+++ src/components/WhatsappButton.js
@@ -27,11 +27,16 @@
 import React from 'react';
 
 function WhatsAppButton({ phoneNumber, shareUrl, image }) {
-    const message = `Check out this product: ${shareUrl}`;
-    const encodedimage = encodeURIComponent(image);
-    console.log('endo', encodedimage);
+    // const message = `Check out this product: ${shareUrl}`;
+    // const encodedimage = encodeURIComponent(image);
+    // console.log('endo', encodedimage);
 
-    const whatsappUrl = `https://wa.me/${phoneNumber}/?text=${encodeURIComponent(message)}&media=${encodedimage}`;
+    // const whatsappUrl = `https://wa.me/${phoneNumber}/?text=${encodeURIComponent(message)}&media=${encodedimage}`;
+
+
+    const message = `${image}\nCheck out this product: ${shareUrl}`;
+    
+    const whatsappUrl = `https://wa.me/${phoneNumber}/?text=${encodeURIComponent(message)}`;
 
     return (
         <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
