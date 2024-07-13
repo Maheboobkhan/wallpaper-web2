6f85823ae76e6259cebfbf354a800176ddb1483d
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-07-13 22:18:28 +0530

    metadat changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,21 +7,16 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  // open
   openGraph: {
     title: 'Next.js',
     description: 'The React Framework for the Web',
     siteName: 'Next.js',
     images: [
       {
-        url: 'https://loremflickr.com/cache/resized/65535_53044203699_1d9ed2a58e_z_640_480_nofilter.jpg',
+        url: '/samp.jpg',
         width: 800,
         height: 600,
-      },
-      {
-        url: 'https://loremflickr.com/cache/resized/65535_53044203699_1d9ed2a58e_z_640_480_nofilter.jpg',
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
       },
     ],
     locale: 'en_US',
